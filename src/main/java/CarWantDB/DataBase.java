c8dcb490157a042299414b28a8f8829a59bc389a
Yevgeniy <yevgeniy.semashko@gmail.com>
2015-07-22 09:32:04 +0300

    New commit 3

--- src/main/java/CarWantDB/DataBase.java
+++ src/main/java/CarWantDB/DataBase.java
@@ -6,23 +6,25 @@ import java.sql.*;
  */
 public class DataBase{
 
-    public void connect () {
-
+    public void connect (String registrationEmail) {
+    //Initialize the DB connection
         try {
             String URL = "jdbc:mysql://rabbit.devplatform1.com/carwant";
-            String USER = "kravchuk";
-            String PASS = "As73cS";
+            String USER = "";
+            String PASS = "";
             Class.forName("org.jdbc.mysql.Driver");
             Connection conn = DriverManager.getConnection(URL, USER, PASS);
 
-
+            // SQL statement
             Statement stmt = conn.createStatement();
-            int rs1 = stmt.executeUpdate("UPDATE `carwant`.`User`SET`status`='1' WHERE`User`.`id`=40");
+            int rs1 = stmt.executeUpdate("UPDATE `carwant`.`User`SET`status`='1' WHERE email = " + registrationEmail   );
 
-            ResultSet rs = stmt.executeQuery("SELECT email,status FROM User WHERE email = 'f480954@trbvm.com'");
-            while (rs.next()) {
+            //ResultSet rs = stmt.executeQuery("SELECT email,status FROM User WHERE email = 'f480954@trbvm.com'");
+            // Print the results
+            /*while (rs.next()) {
                 System.out.println(rs.getRow() + ". " + rs.getString("email") + "\t" + rs.getInt("status"));
-            }
+            }*/
+            // Close connection
             stmt.close();
         }
             catch(Exception e){
